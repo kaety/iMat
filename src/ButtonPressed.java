985ef34ec3a0259656bc786eeeaa7699de630491
Anton Johansson <tomaten666@gmail.com>
2013-02-23 15:36:51 +0100

    Wrapper för buttonEvents

---
+++ src/ButtonPressed.java
@@ -0,0 +1,16 @@
+
+public class ButtonPressed {
+	private String ButtonId;
+	
+	public ButtonPressed(String id){
+		setButtonId(id);
+	}
+
+	public String getButtonId() {
+		return ButtonId;
+	}
+
+	private void setButtonId(String buttonId) {
+		ButtonId = buttonId;
+	}
+}
