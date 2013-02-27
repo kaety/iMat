7610b578679dc056ad21e85d46a5418d5179d92a
PerThoresson <pthoresson@gmail.com>
2013-02-27 20:12:19 +0100

    Lagt till DetailedFoodView och GuestStart

--- src/cards/DetailedFoodView.java
+++ src/cards/DetailedFoodView.java
@@ -3,6 +3,16 @@ package cards;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import javax.swing.DefaultComboBoxModel;
 
 public class DetailedFoodView extends JPanel {
 
@@ -11,8 +21,100 @@ public class DetailedFoodView extends JPanel {
 	 */
 	public DetailedFoodView() {
 		
-		setBackground(Color.YELLOW);
+		setBackground(Color.WHITE);
+		
+		JLabel picLabel = new JLabel("BILD");
+		picLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JLabel priceLabel = new JLabel("Pris");
+		priceLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JPanel panel = new JPanel();
+		
+		JLabel productLabel = new JLabel("Matvara");
+		productLabel.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		productLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel lblNringsinnehll = new JLabel("N\u00E4ringsinneh\u00E5ll:");
+		lblNringsinnehll.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JButton hideProduct = new JButton("D\u00F6lj vara");
+		
+		JButton backButton = new JButton("Tillbaka");
+		
+		JButton addButton = new JButton("L\u00E4gg Till");
+		
+		JComboBox ammountCombo = new JComboBox();
+		ammountCombo.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 430, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(99)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(priceLabel)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(picLabel)
+							.addGap(107)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(productLabel)
+								.addComponent(lblNringsinnehll))))
+					.addContainerGap(135, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(hideProduct)
+					.addGap(236)
+					.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(addButton, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(backButton)
+					.addContainerGap(351, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(backButton)
+							.addPreferredGap(ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+							.addComponent(picLabel))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(productLabel, GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
+							.addGap(11)
+							.addComponent(lblNringsinnehll)))
+					.addGap(55)
+					.addComponent(priceLabel)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(hideProduct, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(addButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(10)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel recipeLabel1 = new JLabel("Recept");
+		recipeLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(recipeLabel1);
+		
+		JLabel recipeLabel2 = new JLabel("Recept");
+		recipeLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(recipeLabel2);
+		
+		JLabel recipeLabel3 = new JLabel("Recept");
+		recipeLabel3.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(recipeLabel3);
+		setLayout(groupLayout);
 
 	}
-
 }
--- src/cards/GuestStart.java
+++ src/cards/GuestStart.java
@@ -3,6 +3,13 @@ package cards;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class GuestStart extends JPanel {
 
@@ -11,7 +18,103 @@ public class GuestStart extends JPanel {
 	 */
 	public GuestStart() {
 		
-		setBackground(Color.BLUE);
+		setBackground(Color.WHITE);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		panel.setBackground(Color.WHITE);
+		
+		JLabel recipePicLabel1 = new JLabel("Bild");
+		recipePicLabel1.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JLabel recipePicLabel2 = new JLabel("Bild");
+		recipePicLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel recipePicLabel3 = new JLabel("Bild");
+		recipePicLabel3.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel label_3 = new JLabel("Recept");
+		label_3.setVerticalAlignment(SwingConstants.TOP);
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 432, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(25)
+					.addComponent(recipePicLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(167)
+					.addComponent(recipePicLabel2, GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
+					.addGap(169)
+					.addComponent(recipePicLabel3, GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
+					.addGap(23))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(172)
+					.addComponent(label_3, GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+					.addGap(172))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGap(0, 114, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(label_3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(40)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(recipePicLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(recipePicLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(recipePicLabel2, GroupLayout.DEFAULT_SIZE, 14, Short.MAX_VALUE))
+					.addGap(46))
+		);
+		panel.setLayout(gl_panel);
+		
+		JLabel lblSomIcaFast = new JLabel("Som ICA, fast b\u00E4ttre!");
+		lblSomIcaFast.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSomIcaFast.setVerticalAlignment(SwingConstants.TOP);
+		lblSomIcaFast.setFont(new Font("Tahoma", Font.PLAIN, 29));
+		
+		JLabel picLabel1 = new JLabel("BILD");
+		picLabel1.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		JLabel picLabel3 = new JLabel("BILD");
+		picLabel3.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel picLabel2 = new JLabel("BILD");
+		picLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(12)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 434, Short.MAX_VALUE)
+					.addGap(8))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(45)
+					.addComponent(picLabel1, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+					.addGap(137)
+					.addComponent(picLabel2, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+					.addGap(146)
+					.addComponent(picLabel3, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+					.addGap(58))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(84)
+					.addComponent(lblSomIcaFast, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(98))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblSomIcaFast)
+					.addGap(62)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(picLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(picLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(picLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(64)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 114, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		setLayout(groupLayout);
 
 	}
 
