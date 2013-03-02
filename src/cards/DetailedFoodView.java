239d9228920c60f498a3ee84d42e5f5e0eb984c2
PerThoresson <pthoresson@gmail.com>
2013-03-02 13:36:35 +0100

    update av detailedFoodView

--- src/cards/DetailedFoodView.java
+++ src/cards/DetailedFoodView.java
@@ -20,6 +20,7 @@ import javax.swing.SwingConstants;
 
 import se.chalmers.ait.dat215.project.IMatDataHandler;
 import se.chalmers.ait.dat215.project.Product;
+import javax.swing.JTextPane;
 
 public class DetailedFoodView extends JPanel {
 	Product activeProduct;
@@ -34,15 +35,18 @@ public class DetailedFoodView extends JPanel {
 	JLabel recipeLabel1;
 	JLabel recipeLabel2;
 	JLabel recipeLabel3;
+	private JTextPane txtpnPerGram;
 	
 	public DetailedFoodView() {
 		
 		setBackground(Color.WHITE);
 		
 		picLabel = new JLabel();
-		picLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		picLabel.setText("BILD");
+		picLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		priceLabel = new JLabel("Pris");
+		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		priceLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		panel = new JPanel();
@@ -52,9 +56,8 @@ public class DetailedFoodView extends JPanel {
 		productLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		
 		JLabel lblNringsinnehll = new JLabel("N\u00E4ringsinneh\u00E5ll:");
+		lblNringsinnehll.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblNringsinnehll.setHorizontalAlignment(SwingConstants.RIGHT);
-		
-		JButton hideProduct = new JButton("D\u00F6lj vara");
 		
 		JButton backButton = new JButton("Tillbaka");
 		
@@ -62,58 +65,81 @@ public class DetailedFoodView extends JPanel {
 		
 		JComboBox ammountCombo = new JComboBox();
 		ammountCombo.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+		
+		JLabel lblNewLabel = new JLabel("Produktbeskrivning:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		
+		JTextPane txtpnLoremIpsumDolor = new JTextPane();
+		txtpnLoremIpsumDolor.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtpnLoremIpsumDolor.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisl neque, ultricies a pellentesque eget, ullamcorper ut lacus. Fusce ac sapien erat, quis viverra lacus.");
+		
+		txtpnPerGram = new JTextPane();
+		txtpnPerGram.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtpnPerGram.setText("Per 100 gram\r\n\r\nKcal/KJ\t260/900\r\nProtein\t12 gr\r\nKolhydrater\t8 gr\r\nFett\t10 gr\r\n");
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 727, Short.MAX_VALUE)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(panel, GroupLayout.DEFAULT_SIZE, 433, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(99)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(priceLabel)
+							.addComponent(picLabel, GroupLayout.PREFERRED_SIZE, 245, GroupLayout.PREFERRED_SIZE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
 								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(picLabel)
-									.addGap(107)
+									.addGap(18)
+									.addComponent(backButton)
+									.addGap(82)
 									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(productLabel)
-										.addComponent(lblNringsinnehll)))))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(80)
-							.addComponent(hideProduct)
-							.addGap(166)
-							.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(addButton, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(backButton)))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGap(22)
+											.addComponent(priceLabel))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(productLabel)
+											.addGap(82)
+											.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(addButton, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)))
+									.addGap(33))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(10)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(txtpnLoremIpsumDolor, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblNewLabel))
+									.addPreferredGap(ComponentPlacement.RELATED, 105, Short.MAX_VALUE)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(txtpnPerGram, GroupLayout.PREFERRED_SIZE, 184, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblNringsinnehll))
+									.addGap(3)))))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(backButton)
-							.addPreferredGap(ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
-							.addComponent(picLabel))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(productLabel, GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(productLabel)
+								.addComponent(addButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(priceLabel)
+							.addGap(32)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel)
+								.addComponent(lblNringsinnehll))
 							.addGap(11)
-							.addComponent(lblNringsinnehll)))
-					.addGap(55)
-					.addComponent(priceLabel)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(addButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(ammountCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(hideProduct, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(10)
-					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(txtpnLoremIpsumDolor, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtpnPerGram, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE))
+							.addGap(28))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(backButton)
+								.addComponent(picLabel, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		panel.setLayout(new GridLayout(1, 0, 0, 0));
