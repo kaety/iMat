0a548ab7d9b284ee199eb3c301354d17fa113cc4
Roy Nard <roy.nard@gmail.com>
2013-03-02 16:55:00 +0100

    Städade samt la till färgen "BASE_LIGHT"

--- src/gui/IMatColors.java
+++ src/gui/IMatColors.java
@@ -6,20 +6,26 @@ package gui;
 import java.awt.Color;
 
 /**
- * @author tortal
+ * @author grp30
  * 
  */
-public class IMatColors extends Color {
+public final class IMatColors {
 	private static final long serialVersionUID = 1L;
 
 	// NON-LAZY INIT
-	public static Color BASE = new Color(212, 87, 1);	//Orange-y
-	public static Color BACKGROUND = Color.WHITE;
-
 	
-	//	Private no-args constructor
-	private IMatColors() {
-		super(255, 255, 255);
+	/** Base Color. All other colors should be related to the base color. */
+	public static final Color BASE = new Color(212, 87, 1);
+
+	/**	Lighter version of Base color */
+	public static final Color BASE_LIGHT = new Color(254, 202, 114);
+	
+	/** Background */
+	public static final Color BACKGROUND = Color.WHITE;
+	
+	/** Prevent construction */
+	private IMatColors(){
+		//Do nothing.
 	}
 
 }
