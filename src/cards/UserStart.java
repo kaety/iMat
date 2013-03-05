0827f5a69c87b748e6e3e05f83cf300380ba1ca6
Anton Johansson <tomaten666@gmail.com>
2013-03-05 21:11:28 +0100

    USERstart tillfixad

--- src/cards/UserStart.java
+++ src/cards/UserStart.java
@@ -14,6 +14,12 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
 import javax.swing.border.LineBorder;
 
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class UserStart extends JPanel {
 
 	/**
@@ -92,76 +98,186 @@ public class UserStart extends JPanel {
 		);
 		panel_1.setLayout(gl_panel_1);
 		
-		JLabel lblNewLabel = new JLabel("Tidigare Ink\u00F6p");
+		JLabel lblNewLabel = new JLabel("Veckans Medlemsvaror");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		
-		JLabel buyPicLabel1 = new JLabel("Bild");
-		buyPicLabel1.setToolTipText("Tryck h\u00E4r f\u00F6r att titta p\u00E5 kvitto och l\u00E4gga till varor igen");
-		buyPicLabel1.setHorizontalAlignment(SwingConstants.LEFT);
-		
-		JLabel buyPicLabel2 = new JLabel("Bild");
-		buyPicLabel2.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JLabel buyPicLabel3 = new JLabel("Bild");
-		buyPicLabel3.setHorizontalAlignment(SwingConstants.RIGHT);
-		
-		JLabel priceLabel1 = new JLabel("Pris");
-		priceLabel1.setVerticalAlignment(SwingConstants.BOTTOM);
-		priceLabel1.setHorizontalAlignment(SwingConstants.LEFT);
-		
-		JLabel priceLabel2 = new JLabel("Pris");
-		priceLabel2.setVerticalAlignment(SwingConstants.BOTTOM);
-		priceLabel2.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JLabel priceLabel3 = new JLabel("Pris");
-		priceLabel3.setVerticalAlignment(SwingConstants.BOTTOM);
-		priceLabel3.setHorizontalAlignment(SwingConstants.RIGHT);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 255, 255));
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(28)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(buyPicLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(1))
-						.addComponent(priceLabel1))
-					.addGap(160)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(buyPicLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(1))
-						.addComponent(priceLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(153)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(buyPicLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(1))
-						.addComponent(priceLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(36))
+					.addContainerGap()
+					.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 741, Short.MAX_VALUE)
+					.addGap(11))
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(136)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 124, Short.MAX_VALUE)
-					.addGap(139))
+					.addGap(11)
+					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 741, Short.MAX_VALUE)
+					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addGap(16)
 					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(41)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(buyPicLabel1, GroupLayout.DEFAULT_SIZE, 14, Short.MAX_VALUE)
-						.addComponent(buyPicLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(buyPicLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(43)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(priceLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(priceLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(priceLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
+		panel_2.setLayout(new MigLayout("", "[269.00,grow][58.00][0.00][334.00,grow][grow]", "[grow][grow]"));
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_9, "cell 0 0,grow");
+		
+		//PRESS ON IMAGE
+		
+		JLabel buyPicLabel1 = new JLabel(IMatDataHandler.getInstance().getImageIcon(IMatDataHandler.getInstance().getProduct(34), 150, 150));
+		buyPicLabel1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+		buyPicLabel1.setToolTipText("Tryck här för att komma till veckans varor");
+		GroupLayout gl_panel_9 = new GroupLayout(panel_9);
+		gl_panel_9.setHorizontalGroup(
+			gl_panel_9.createParallelGroup(Alignment.LEADING)
+				.addComponent(buyPicLabel1, GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
+		);
+		gl_panel_9.setVerticalGroup(
+			gl_panel_9.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_9.createSequentialGroup()
+					.addComponent(buyPicLabel1)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_9.setLayout(gl_panel_9);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_8, "cell 3 0,grow");
+		
+		JLabel buyPicLabel2 = new JLabel(IMatDataHandler.getInstance().getImageIcon(IMatDataHandler.getInstance().getProduct(78), 150, 150));
+		
+		//PRESS ON IMAGE
+		
+		buyPicLabel2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		buyPicLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+		GroupLayout gl_panel_8 = new GroupLayout(panel_8);
+		gl_panel_8.setHorizontalGroup(
+			gl_panel_8.createParallelGroup(Alignment.TRAILING)
+				.addComponent(buyPicLabel2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
+		);
+		gl_panel_8.setVerticalGroup(
+			gl_panel_8.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_panel_8.createSequentialGroup()
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(buyPicLabel2))
+		);
+		panel_8.setLayout(gl_panel_8);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_6, "cell 4 0,grow");
+		
+		JLabel buyPicLabel3 = new JLabel(IMatDataHandler.getInstance().getImageIcon(IMatDataHandler.getInstance().getProduct(55), 150, 150));
+		buyPicLabel3.addMouseListener(new MouseAdapter() {
+			
+			//PRESS ON IMAGE
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		GroupLayout gl_panel_6 = new GroupLayout(panel_6);
+		gl_panel_6.setHorizontalGroup(
+			gl_panel_6.createParallelGroup(Alignment.LEADING)
+				.addComponent(buyPicLabel3, GroupLayout.DEFAULT_SIZE, 319, Short.MAX_VALUE)
+		);
+		gl_panel_6.setVerticalGroup(
+			gl_panel_6.createParallelGroup(Alignment.LEADING)
+				.addComponent(buyPicLabel3, GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+		);
+		panel_6.setLayout(gl_panel_6);
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_10, "cell 0 1,grow");
+		Product p1 = IMatDataHandler.getInstance().getProduct(34);
+		Product p2 = IMatDataHandler.getInstance().getProduct(55);
+		Product p3 = IMatDataHandler.getInstance().getProduct(78);
+		JLabel priceLabel1 = new JLabel(p1.getName() + " för " + p1.getPrice() + p1.getUnit());
+		priceLabel1.setVerticalAlignment(SwingConstants.BOTTOM);
+		priceLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+		GroupLayout gl_panel_10 = new GroupLayout(panel_10);
+		gl_panel_10.setHorizontalGroup(
+			gl_panel_10.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_10.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel1, GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_panel_10.setVerticalGroup(
+			gl_panel_10.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_10.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel1)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_10.setLayout(gl_panel_10);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_7, "cell 3 1,grow");
+		
+		JLabel priceLabel2 = new JLabel(p3.getName() + " för " + p3.getPrice() + p3.getUnit());
+		priceLabel2.setVerticalAlignment(SwingConstants.BOTTOM);
+		priceLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+		GroupLayout gl_panel_7 = new GroupLayout(panel_7);
+		gl_panel_7.setHorizontalGroup(
+			gl_panel_7.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_7.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel2, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_panel_7.setVerticalGroup(
+			gl_panel_7.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_7.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel2)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_7.setLayout(gl_panel_7);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(255, 255, 255));
+		panel_2.add(panel_5, "cell 4 1,grow");
+		
+		JLabel priceLabel3 = new JLabel(p2.getName() + " för " + p2.getPrice() + p2.getUnit());
+		priceLabel3.setVerticalAlignment(SwingConstants.BOTTOM);
+		priceLabel3.setHorizontalAlignment(SwingConstants.CENTER);
+		GroupLayout gl_panel_5 = new GroupLayout(panel_5);
+		gl_panel_5.setHorizontalGroup(
+			gl_panel_5.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel3, GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_panel_5.setVerticalGroup(
+			gl_panel_5.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_5.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(priceLabel3)
+					.addContainerGap(14, Short.MAX_VALUE))
+		);
+		panel_5.setLayout(gl_panel_5);
 		panel.setLayout(gl_panel);
 		setLayout(groupLayout);
 
